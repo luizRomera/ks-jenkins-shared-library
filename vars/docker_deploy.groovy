92266ffbb10fa663b83c69d2d8d7250282256f30
Luiz Rubens <luiz.rubens.romera@gmail.com>
2024-02-05 23:00:14 -0300

    Ajusta script docker_deploy

--- vars/docker_deploy.groovy
+++ vars/docker_deploy.groovy
@@ -1,8 +1,9 @@
 def call(String microservice=null) {
     
     if ("${microservice}" == "publisher-manager"){
+        sh 'docker container prune -f'
         sh 'docker compose down'
-        sh 'docker compose up -d || true'
+        sh 'docker compose up -d'
     } else {
         sh 'exit 0'
     }
