177f9112e1d0666bcc034619e3a27b4afb4b6051
Luiz Rubens <luiz.rubens.romera@gmail.com>
2024-02-05 22:34:03 -0300

    Adiciona script de docker_deploy

---
+++ vars/docker_deploy.groovy
@@ -0,0 +1,6 @@
+def call() {
+    
+    sh 'docker compose down'
+    sh 'docker compose up -d'
+
+}
--- vars/post.groovy
+++ vars/post.groovy
@@ -1,6 +1,5 @@
 def call(String microservice=null, String host=null) {
     
-
     if("${microservice}" == "publisher-manager"){
         echo "Build successful! Artifact generated and published."
         echo "Aplication running and avaliable on: http://${host}:8080/publisher"
