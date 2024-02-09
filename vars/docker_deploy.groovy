79ee58eb8cf40159a28da890589a3996c88a5096
Luiz Rubens <luiz.rubens.romera@gmail.com>
2024-02-09 07:59:14 -0300

    Altera comando de up dos containers

--- vars/docker_deploy.groovy
+++ vars/docker_deploy.groovy
@@ -3,7 +3,7 @@ def call(String microservice=null) {
     if ("${microservice}" == "publisher-manager"){
         sh 'docker container prune -f'
         sh 'docker compose kill'
-        sh 'docker compose up -d publisher-manager'
+        sh 'docker compose up -d'
     } else {
         sh 'exit 0'
     }
