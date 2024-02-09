48a6c3bdfd908ff5e32badbc938af6167018f783
Luiz Rubens <luiz.rubens.romera@gmail.com>
2024-02-09 07:58:56 -0300

    Força stop dos containers

--- vars/docker_deploy.groovy
+++ vars/docker_deploy.groovy
@@ -2,8 +2,7 @@ def call(String microservice=null) {
     
     if ("${microservice}" == "publisher-manager"){
         sh 'docker container prune -f'
-        sh 'docker compose down'
-        sh 'docker rm mysql'
+        sh 'docker compose kill'
         sh 'docker compose up -d publisher-manager'
     } else {
         sh 'exit 0'
