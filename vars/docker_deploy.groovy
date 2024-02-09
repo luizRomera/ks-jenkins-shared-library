0527126af8ad1dc15c03a535bf761686ff2ee147
Luiz Rubens <luiz.rubens.romera@gmail.com>
2024-02-09 08:10:01 -0300

    Ajusta comandos docker_deploy

--- vars/docker_deploy.groovy
+++ vars/docker_deploy.groovy
@@ -2,7 +2,7 @@ def call(String microservice=null) {
     
     if ("${microservice}" == "publisher-manager"){
         sh 'docker container prune -f'
-        sh 'docker compose kill'
+        sh 'docker compose down && docker rm -f mysql publisher-manager'
         sh 'docker compose up -d'
     } else {
         sh 'exit 0'
